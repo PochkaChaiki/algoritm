5a4efbbd10b7b0b08ce16a3885e49e7d4c4870ed
Егор Костенко <pochka.chaiki@gmail.com>
2022-09-16 00:25:33 +0300

    Fixed some potential trouble places and added add-functions

--- Kostenkolr2/Kostenko.cpp
+++ Kostenkolr2/Kostenko.cpp
@@ -7,19 +7,21 @@
 
 
 struct Pipe {
-    // int id;
+    unsigned int ID;
     bool status = false;
     int length = 0;
     int diameter = 0;
 };
 
 struct CompressorStation {
-    // int id;
-    int efficiency;
+    unsigned int ID;
+    double efficiency;
     int shopsAmount = 0;
     std::vector<int> shops;
 };
 
+int addPipe(std::vector<Pipe>& pipeline, int length, int diameter, bool status);
+int addCS(std::vector<CompressorStation>& company, int shopsAmount, std::vector<int>& shops);
 int checkInput(std::istream& in);
 void showObjects(Pipe& pipe, CompressorStation& station);
 void editPipe(Pipe& pipe);
@@ -31,13 +33,15 @@ void loadCompany(Pipe& pipe, CompressorStation& station);
 
 
 int main(){
-    int option(-1);
-    Pipe myPipe;
-    CompressorStation myCompressorStation;
-    std::cout<<"Create your own oil producing company"<<std::endl;
+    int option(-1), pipeId, CSId;
+    std::vector<Pipe> myPipeline;
+    std::vector<CompressorStation> myGasCompany;
+
+    std::cout<<"Create your own gas producing company"<<std::endl;
     std::cout<<"#---------------------------------------------#"<<std::endl;
+
     while (option){
-        std::cout<<"Choose menu's option:\n1.Add a pipe;\n2.Add a compressor station;\n3.Show all objects;\n4.Edit a pipe;\n5.Edit a compressor station;\n6.Save oil company;\n7.Load oil companys\n0.Close program"<<std::endl;
+        std::cout<<"Choose menu's option:\n1.Add a pipe;\n2.Add a compressor station;\n3.Show all objects;\n4.Edit a pipe;\n5.Edit a compressor station;\n6.Save gas company;\n7.Load gas companys\n0.Close program"<<std::endl;
         option = checkInput(std::cin);
         if (option == -1)
             continue;
@@ -50,31 +54,60 @@ int main(){
             system("CLS");
             continue;
         }
-        // std::string inp
         
-         // Checking option ------------------------------------------------------------------------------------------------------------------------
+// Checking option ------------------------------------------------------------------------------------------------------------------------
 
         if (option == 1){
+    //Adding pipe ------------------------------------------------------------------
             std::cout<<"Input pipe\'s parameteres (length, diameter, status): ";
-            std::cin>>myPipe.length>>myPipe.diameter>>myPipe.status;
-            std::cout<<std::endl;
-            
-        } else if (option == 2){
-            std::cout<<"Input compressor station shops' parameteres (each shop's status (\"1\" if it's \"in work\", and \"0\" if it is not)): ";
-            int status;
+            int length = checkInput(std::cin), diameter = checkInput(std::cin);
+            bool status = checkInput(std::cin);
+            // std::cin>>length>>diameter>>status;
 
-            while (std::cin>>status || std::cin.fail()){
-                status = checkInput(std::cin);
-                myCompressorStation.shops.push_back(status);
-            }
+            pipeId = addPipe(myPipeline, length, diameter, status);
+
+            std::cout<<"Id currently used pipe: "<<pipeId<<std::endl;
             std::cout<<std::endl;
+
+        } else if (option == 2){
+    //Adding compressor station ------------------------------------------------------
+            int shopsAmount;
+            std::vector<int> shops;
+    
+            std::cout<<"Input compressor station shops' amount: ";
+            shopsAmount = checkInput(std::cin);
+            std::cout<<"Input compressor station each shop's status - \"1\" if it's \"in work\", and \"0\" if it is not (REMINDER: if you pass amount of statuses that is more than you wrote step back, extra statuses won't be passed): ";
+            for (int i(0); i<shopsAmount; ++i){
+                int status = checkInput(std::cin);
+                status = (status > 1) ? 1 : status;
+                if (status > -1)
+                    shops.push_back(status);
+                else
+                    shops.push_back(0);
+            }
+
+            CSId = addCS(myGasCompany, shopsAmount, shops);
+            myGasCompany[CSId].efficiency = (double)std::count(shops.begin(), shops.end(), 1) / shopsAmount;
+
+            std::cin.clear();
+            std::cin.ignore(INT_MAX, '\n');
+            std::cout<<"Id currently used compressor station: "<<CSId<<std::endl;
+            std::cout<<std::endl;
+
         } else if (option == 3){
-            showObjects(myPipe, myCompressorStation);
+    //Showing all existing objects -----------------------------------------------------
+            showObjects(myPipeline[pipeId], myGasCompany[CSId]);
+
+        } else if (option == 4){
+
         }
+
+        system("pause");
         system("CLS");
     }
     return 0;
 }
+
 
 int checkInput(std::istream& in){
     int checkingValue;
@@ -94,14 +127,17 @@ int checkInput(std::istream& in){
 }
 
 void showObjects(Pipe& pipe, CompressorStation& station){
+    std::cout<<"--------------------------------------------------------"<<std::endl;
     std::cout<<"Pipe parameteres: "<<std::endl;
+    std::cout<<"Pipe ID: "<<pipe.ID<<std::endl;
     std::cout<<"Pipe's status: "<<(pipe.status?"In work":"Repairing")<<std::endl;
     std::cout<<"Pipe's length: "<<pipe.length<<std::endl;
     std::cout<<"Pipe's diameter: "<<pipe.diameter<<std::endl;
 
     std::cout<<"--------------------------------------------------------"<<std::endl;
     std::cout<<"Compressor Station's (CS) parameteres: "<<std::endl;
-    std::cout<<"CS's efficiency: "<<station.efficiency<<std::endl;
+    std::cout<<"CS ID: "<<station.ID<<std::endl;
+    std::cout<<"CS's efficiency: "<<station.efficiency * 100<<"%"<<std::endl;
     std::cout<<"CS's shops amount: "<<station.shops.size()<<std::endl;
     std::cout<<"CS's working shops amount: "<<std::count(station.shops.begin(), station.shops.end(), 1)<<std::endl;
     std::cout<<"CS's each shop's status: "<<std::endl;
@@ -111,4 +147,13 @@ void showObjects(Pipe& pipe, CompressorStation& station){
     system("pause");
 }
 
+int addPipe(std::vector<Pipe>& pipeline, int length, int diameter, bool status){
+    pipeline.push_back({(unsigned int)pipeline.size(), status, length, diameter});
+    return pipeline.size()-1;
+}
+
+int addCS(std::vector<CompressorStation>& company, int shopsAmount, std::vector<int>& shops){
+    company.push_back({(unsigned int)company.size(), 0, shopsAmount, shops});
+    return company.size()-1;
+}
 // std::cout<<"Choose menu's option:\n1.Add a pipe;\n2.Add a compressor station;\n3.Show all objects;\n4.Edit a pipe;\n5.Edit a compressor station;\n6.Save oil company;\n7.Load oil companys\n0.Close program"<<std::endl;
